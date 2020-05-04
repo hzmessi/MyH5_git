89166c40123662edfd0e8515e003af20badb6755
tony <hz_yuanjiang>
2020-05-04 16:32:45 +0800

    modify

--- random_js.js
+++ random_js.js
@@ -1,6 +1,6 @@
-var inputNumber = prompt("Your Guessed Number:")
-var random = Math.random()*6
-random = Math.floor(random) + 1
+var inputNumber = prompt("Your Guessed Number:");
+var random = Math.random()*6;
+random = Math.floor(random) + 1;
 // document.write(random)
 if (inputNumber == random){
     alert("You win!")
